5ab068bd3ef6920cc156ea2addce3de71c51e19b
farazhashmi21 <farazhashmi9000@yahoo.com>
2024-07-22 15:09:50 +0000

    Write A Hello World Program in C Language.

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+/** Write A Hello World Program in C Language. **/
+
+#include<stdio.h>
+
+void displ();
+
+int main(){
+	displ();
+	return 0;
+}
+
+void displ(){
+	printf("Assalam-O-Alaiqum World.");
+}
